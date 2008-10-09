3c35a413d3c7fb2aeb3deda6692472040318b026
tedwardo2 <tedwardo2@gmail.com>
2008-10-08 19:34:00 -0700

    got the tee tile overlays working

--- public/javascripts/teerack.js
+++ public/javascripts/teerack.js
@@ -3,3 +3,16 @@ function showVote(shirtId, upOrDown) {
   $('thumb_'+(upOrDown == "up" ? "down" : "up")+'_'+shirtId).removeClassName('voted');
   if (upOrDown == 'down') $('tee_tile_'+shirtId).setOpacity(0.4);
 }
+
+document.observe("dom:loaded", function() {
+  $$('.tee_tile').each(function(tile) {
+    tile.observe('mouseover', function() {
+      this.down('.tee_thumb').setOpacity(0.1);
+      this.down('.tee_info').show();
+    });
+    tile.observe('mouseout', function() {
+      this.down('.tee_info').hide();
+      this.down('.tee_thumb').setOpacity(1);
+    });
+  });
+});
