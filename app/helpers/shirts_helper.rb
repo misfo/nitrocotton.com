18d3b0d3ab9c4fdcd8c04f4794bf85d42e6f5436
tedwardo2 <tedwardo2@gmail.com>
2008-10-10 13:24:08 -0700

    some display changes

--- app/helpers/shirts_helper.rb
+++ app/helpers/shirts_helper.rb
@@ -2,4 +2,8 @@ module ShirtsHelper
   def price(number)
     number % 1 == 0 ? "$#{number.to_i}" : number_to_currency(number)
   end
+  
+  def price_range(min, max)
+    [price(min), price(max)].uniq.join("-")
+  end
 end
