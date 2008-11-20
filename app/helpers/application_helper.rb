4f77fd9768d937ed750e31cf9422278db79a8870
tedwardo2 <tedwardo2@gmail.com>
2008-11-20 14:40:46 -0800

    added footer email link and shirt count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def rated_percentage
-    number_to_percentage(100 * user_session.user.votes.size.to_f / Shirt.count, :precision => 1)
+    number_to_percentage(100 * user_session.user.votes.size.to_f / total_shirt_count, :precision => 1)
+  end
+  
+  def total_shirt_count
+    @total_shirts_count ||= Shirt.count
+  end
+  
+  def last_shirt_added_at
+    @last_shirt_added_at ||= Shirt.maximum(:created_at)
   end
 end
