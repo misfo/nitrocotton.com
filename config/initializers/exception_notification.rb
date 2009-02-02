e0a0a960cac643846755489482c3c7a3bdea14f4
Trent Ogren <togren@iexposure.com>
2009-02-02 14:21:21 -0600

    fixed exception sender address

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,3 +1,3 @@
 ExceptionNotifier.exception_recipients = %w(trent@nitrocotton.com)
-ExceptionNotifier.sender_address = %("nitrocotton exception" <exceptions@nitrocotton.com>)
+ExceptionNotifier.sender_address = %("nitrocotton exception" <robot@nitrocotton.com>)
 
